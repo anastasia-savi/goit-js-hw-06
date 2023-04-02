07377eb6e66c789bc1d2fc450ce1a27b74b273c2
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-04-02 11:09:48 +0300

    hw-js6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector("#validation-input");
 const attribute = Number(textInput.getAttribute("data-length"));
 const inputBlur = (event) => {
-  if (event.currentTarget.value.length >= attribute) {
+  if (event.currentTarget.value.length == attribute) {
     return textInput.classList.add("valid");
   } else {
     return textInput.classList.toggle("invalid");
